2c68473314ba1510b518ef2ec2f3d045a4852c0e
Saksham Thapa <56692835+Saksham010@users.noreply.github.com>
2022-01-13 23:55:32 +0545

    Voting functionality added
    50% + consensus approval

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -10,7 +10,18 @@ contract CrowdFunding{
     uint public raisedAmount;
     uint public deadline;
     uint public minimumContribution;
-
+ 
+    //Request to vote
+    struct Request{
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address=>bool) voted;
+    }
+    mapping(uint=>Request) public request;
+    uint public numofRequest;
 
     constructor(){
         owner = msg.sender;
@@ -41,9 +52,44 @@ contract CrowdFunding{
         //Refunding the contributed amount
         user.transfer(amount);
     }
+    
+    function createRequests(string memory _description, address payable _recipient,uint _value) public{
+        require(msg.sender == owner);
+        Request storage newRequest = request[numofRequest]; //=> Request var -> [0 -> Request block]
+        numofRequest++;
+
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters =0;
+    }
+
+
+    function voteOnRequest(uint requestNo)public {
+        require(msg.sender != owner);
+        require(participant[msg.sender] > 0,"You are not eligible to vote");
+        Request storage newRequest = request[requestNo];
+        require(newRequest.voted[msg.sender] == false, "You have already voted");
+
+        newRequest.voted[msg.sender] = true;
+        newRequest.noOfVoters++;    
+    }
+
+    function payfund(uint requestNo)public{
+        require(msg.sender == owner);
+        require(raisedAmount >= target);
+        Request storage newRequest = request[requestNo];
+        require(newRequest.completed == false,"Already funded");
+        require(newRequest.noOfVoters > (numofparticipants)/2,"Majority approval consensus not reached");
+
+        //Transferring funds
+        newRequest.recipient.transfer(newRequest.value); 
+        newRequest.completed = true;
+    }
 
     function getBalanceContract() public view returns(uint){
         return address(this).balance;
     }
-
+    
 }
